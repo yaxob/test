cb283ad1545878884833f99a0fef87a2b3918d39
xuri <T0Bl4S.durst@gmail.com>
2019-08-29 21:50:39 +0200

    addet jaxob string

--- ConsoleApp1/test/Program.cs
+++ ConsoleApp1/test/Program.cs
@@ -4,14 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace test
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace test {
+    class Program {
+        static void Main(string[] args) {
+            var    jaxob  = "Hallo Ein string";
             string tobias = "hallo Welt";
-            Console.WriteLine(tobias);
+            Console.WriteLine( tobias + "  " + jaxob );
             Console.ReadLine();
         }
     }
